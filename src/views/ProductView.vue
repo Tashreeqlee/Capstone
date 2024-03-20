45015796ff9054b78266a5326e8092a6721a5784
Tashreeqlee <tashyct14@gmail.com>
2024-03-20 12:12:40 +0200

    single product view

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,18 +1,24 @@
 <template>
-    <div v-if="product" class="container-fluid text-center">
-        <h1>{{ product.prodName }}</h1>
-        <img :src="product.prodUrl" :alt="product.prodName" class="img-fluid"/>
-        <p>Price: R {{ product.prodPrice }}</p>
-        <p>Category: {{ product.category }}</p>
-        <p>Description: {{ product.description }}</p>
-        <p>Stock Left: {{ product.quantity }}</p>
+     <div v-if="product" class="card-container">
+    <div class="card">
+      <div class="card-header">
+        <h1 class="card-title">{{ product.prodName }}</h1>
+      </div>
+      <div class="card-body">
+        <img :src="product.prodUrl" :alt="product.prodName" class="card-img-top" />
+        <p class="card-text">Price: R {{ product.prodPrice }}</p>
+        <p class="card-text">Category: {{ product.category }}</p>
+        <p class="card-text">Description: {{ product.description }}</p>
+        <p class="card-text">Stock Left: {{ product.quantity }}</p>
         <button class="btn btn-outline-dark animate__animated animate__bounce animate__delay-3s" @click.prevent="addCart(product)">
-            Add To Cart
-          </button>
+          Add To Cart
+        </button>
+      </div>
     </div>
-    <div v-else class="d-flex justify-content-center">
-        <SpinnerComp/>
-    </div>
+  </div>
+  <div v-else class="d-flex justify-content-center">
+    <SpinnerComp />
+  </div>
 </template>
 
 <script>
@@ -38,7 +44,48 @@ export default {
 }
 </script>
 
-<style  scoped>
+<style scoped>
+.card-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.card {
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+  background-color: #f8f9fa;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+.card-body {
+  padding: 20px;
+}
+
+.card-img-top {
+  width: 100%;
+  height: auto;
+  border-radius: 5px 5px 0 0;
+}
+
+.card-title {
+  margin-top: 0;
+}
+
+.card-text {
+  margin-bottom: 10px;
+}
+
+.btn {
+  margin-top: 15px;
+}
 
 
 </style>
